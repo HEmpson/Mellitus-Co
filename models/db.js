db76dde62047ebd212e41022ba5efc90075dd43b
hempson <hempson@student.unimelb.edu.au>
2022-08-26 08:20:20 +1000

    Feat: started implementing schemas

--- models/db.js
+++ models/db.js
@@ -2,4 +2,20 @@ const mongoose = require('mongoose')
 
 // add schemas
 
+const userSchema = new mongoose.Schema({
+    userFirstName: String,
+    userLastName: String,
+    displayName: String, // idk if we are doing a display name??
+    email: String,
+    status: String,
+    bio: String,
+    friends: [userSchema],
+    files: [postSchema],
+
+})
+
+const postSchema = new mongoose.Schema({
+
+})
+
 module.exports = {}
