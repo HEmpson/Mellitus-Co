5b1687c17744aa027750344c71e3d830103482d9
hempson <hempson@student.unimelb.edu.au>
2022-08-31 11:06:54 +1000

    Feat: renamed files

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -2,6 +2,29 @@ const express = require('express')
 const appRouter = express.Router()
 const appController = require('../controllers/appController')
 
+
+// Authentication middleware
+const isAuthenticated = (req, res, next) => {
+    // If user is not authenticated via passport, redirect to login page
+    if (!req.isAuthenticated()) {
+        return res.redirect('/login')
+    }
+    // Otherwise, proceed to next middleware function
+    return next()
+}
+
+// Set up role based authentication
+const hasRole = (thisRole) => {
+    return (req, res, next) => {
+        if (req.user.role === thisRole) {
+            return next()
+        } else {
+            res.redirect('/login')
+        }
+    }
+}
+
+
 // put routes here
 
 appRouter.get('/', appController.getTestPage)
