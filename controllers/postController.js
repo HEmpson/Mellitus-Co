69cfd04f3cf7cb7f3065eb80df6fbcbd9f62122c
Suren Fernando <suren.fernando2001@gmail.com>
2022-09-04 23:20:35 +1000

    merge changes

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,7 +7,7 @@ const makePost = async (req, res) => {
     const body = req.body
     console.log(body)
     const newPost = new Post({
-        visibility: req.bosy.visibility,
+        visibility: req.body.visibility,
         description: req.body.message,
         fileId: mongoose.Types.ObjectId(req.file.id),
         createdBy: req.user._id,
