bc08e0c8bd9f70d990e7aa1b5b3c32e8d726eb35
bhisa <bhisa@student.unimelb.edu.au>
2022-09-14 11:49:40 +1000

    feat: fixed authentication middleware

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -6,7 +6,7 @@ const appController = require('../controllers/appController')
 const isAuthenticated = (req, res, next) => {
     // If user is not authenticated via passport, redirect to login page
     if (!req.isAuthenticated()) {
-        return res.redirect('/login')
+        return res.redirect('/')
     }
     // Otherwise, proceed to next middleware function
     return next()
@@ -18,7 +18,7 @@ const hasRole = (thisRole) => {
         if (req.user.role === thisRole) {
             return next()
         } else {
-            res.redirect('/login')
+            res.redirect('/')
         }
     }
 }
